2ffa27fcf7e6271a17c62b8b4c54563ee7bc77c5
gildrausNV <nikola.vujcic.001@gmail.com>
2023-10-16 00:22:06 +0200

    SeederController changes

--- network-backend/src/main/java/rs/ac/bg/fon/social_network/seed/SeederController.java
+++ network-backend/src/main/java/rs/ac/bg/fon/social_network/seed/SeederController.java
@@ -2,6 +2,7 @@ package rs.ac.bg.fon.social_network.seed;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SeederController {
 
-//    private final FakeService fakeService;
-//
-//    @GetMapping
-//    public void seed() {
-////        fakeService.seedUsers();
-//        fakeService.seedAdmin();
-//    }
+    private final FakeService fakeService;
+
+    @GetMapping
+    public void seed() {
+//        fakeService.seedUsers();
+        fakeService.seedAdmin();
+    }
 
 }
