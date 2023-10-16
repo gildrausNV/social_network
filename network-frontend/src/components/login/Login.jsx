b5ea60940343fd06056b83ca51fc77f03cce3d0a
gildrausNV <nikola.vujcic.001@gmail.com>
2023-10-16 18:42:10 +0200

    New login post request for 2 factor authentication

--- network-frontend/src/components/login/Login.jsx
+++ network-frontend/src/components/login/Login.jsx
@@ -6,13 +6,15 @@ import { AuthContext } from '../../App';
 
 import './Login.css';
 
-const Login = ({setToken, setId, setIsAdmin, setUserContext}) => {
+const Login = ({ setToken, setId, setIsAdmin, setUserContext }) => {
   const [user, setUser] = useState({
     username: '',
     password: '',
   });
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
+  const [code, setCode] = useState('');
+  const [isSent, setIsSent] = useState(false);
 
   const handleUserChange = (event) => {
     setUser({
@@ -22,37 +24,37 @@ const Login = ({setToken, setId, setIsAdmin, setUserContext}) => {
   };
 
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
 
-    try {
-      const response = await axios.post(
-        'http://localhost:8080/api/v1/auth/login',
-        {
-          username: user.username,
-          password: user.password,
-        }
-      );
+  //   try {
+  //     const response = await axios.post(
+  //       'http://localhost:8080/api/v1/auth/login',
+  //       {
+  //         username: user.username,
+  //         password: user.password,
+  //       }
+  //     );
 
-      const { token, id, role } = response.data;
+  //     const { token, id, role } = response.data;
 
-      localStorage.setItem('id', id);
-      localStorage.setItem('token', token);
-      localStorage.setItem('isAdmin', role==='ADMIN');
-      setToken(token);
-      setId(id);
-      setIsAdmin(role==='ADMIN');
-      setUserContext({
-        id,
-        token,
-        isAdmin : role==='ADMIN'
-      });
-      navigate('/main');
-    } catch (error) {
-      console.error('Login failed:', error);
-      setMessage('Login failed');
-    }
-  };
+  //     localStorage.setItem('id', id);
+  //     localStorage.setItem('token', token);
+  //     localStorage.setItem('isAdmin', role==='ADMIN');
+  //     setToken(token);
+  //     setId(id);
+  //     setIsAdmin(role==='ADMIN');
+  //     setUserContext({
+  //       id,
+  //       token,
+  //       isAdmin : role==='ADMIN'
+  //     });
+  //     navigate('/main');
+  //   } catch (error) {
+  //     console.error('Login failed:', error);
+  //     setMessage('Login failed');
+  //   }
+  // };
 
   useEffect(() => {
     localStorage.removeItem('token');
@@ -64,39 +66,91 @@ const Login = ({setToken, setId, setIsAdmin, setUserContext}) => {
     });
   }, [])
 
+  const login = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/api/v1/auth/login',
+        {
+          username: user.username,
+          password: user.password,
+        }
+      );
+      console.log(response)
+      setIsSent(true);
+      setMessage(response.data.token);
+    } catch (error) {
+      console.error('Login failed:', error);
+      setMessage('Login failed');
+    }
+  }
+
+  const verify = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/api/v1/auth/verify',
+        {
+          username: user.username,
+          verificationCode: code
+        }
+      );
+      console.log(response)
+      const { token, id, role } = response.data;
+
+      localStorage.setItem('id', id);
+      localStorage.setItem('token', token);
+      localStorage.setItem('isAdmin', role === 'ADMIN');
+      setToken(token);
+      setId(id);
+      setIsAdmin(role === 'ADMIN');
+      setUserContext({
+        id,
+        token,
+        isAdmin: role === 'ADMIN'
+      });
+      navigate('/main');
+    } catch (error) {
+      console.error('Login failed:', error);
+      setMessage('Login failed');
+    }
+  }
+
+
   return (
     <div className="login">
       <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={user.username}
-            onChange={handleUserChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={user.password}
-            onChange={handleUserChange}
-            required
-          />
-        </div>
-        <button type="submit" className='login-btn'>Login</button>
-        
-      </form>
+      <div className="form-group">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={user.username}
+          onChange={handleUserChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={user.password}
+          onChange={handleUserChange}
+          required
+        />
+      </div>
+      <button className='login-btn' onClick={() => login()}>Login</button>
+
       <p>
         Don't have an account? <Link to="/register">Create one</Link>
       </p>
-      {message!=='' && <div className="error-message">{message}</div>}
+      {message !== '' && <div className="error-message">{message}</div>}
+      {isSent && <>
+        <label htmlFor="">Google Authenticator Verification Code</label>
+        <input type='text' name='code' onChange={(e) => setCode(e.target.value)} />
+        <button onClick={() => verify()}>Verify</button>
+      </>}
     </div>
   );
 };
